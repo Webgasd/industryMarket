c5221ad1b866cfeed2a05bde1d138e61077781d0
wenghaonan <wx_ae5873b9e90e46448f55e0851286313b@git.code.tencent.com>
2020-07-22 10:48:00 +0800

    update application

--- src/main/java/com/upc/industry/IndustryApplication.java
+++ src/main/java/com/upc/industry/IndustryApplication.java
@@ -1,13 +1,9 @@
 package com.upc.industry;
 
-import com.upc.industry.dao.UserDOMapper;
-import com.upc.industry.dataobject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"com.upc.industry"})
@@ -18,7 +14,6 @@ public class IndustryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IndustryApplication.class, args);
-        System.out.println("hola world");
+        System.out.println("app is processing!");
     }
-
 }
